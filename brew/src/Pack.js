9f5e5af192532ac7fc4a413364005e174db8706b
Alexander Belov <callmistercat@gmail.com>
2019-06-19 12:22:53 +0300

    oPack translated to russian.

--- brew/src/Pack.js
+++ brew/src/Pack.js
@@ -73,13 +73,10 @@ class Pack extends Component {
                     <Section id={'omnipack'} ref={this.packRef}>
                         <SectionHeader icon={oPack}>Pack</SectionHeader>
                         <SectionDescription>
-                            Setup your palletization robot in a few easy steps. It is fast, flexible and allows you to track the status of each robot at any time.
+                            Настройте свой пост паллетизации за насколько простых шагов. OmniPack позволяет быстро изменить параметры раскладки и переналадить робота без специалиста.
                         </SectionDescription>
-                        <SectionCaption>
-                            OmniPack supports most of all industrial robots.
-                        </SectionCaption>
                         <div className="download-offer">
-                            <a href={'https://drive.google.com/file/d/120sqFITfTkIN_iXdc7fYcW5UFgbJyHeC/view?usp=sharing'} target={'_blank'}>Download Specification</a>
+                            <a href={'https://drive.google.com/file/d/120sqFITfTkIN_iXdc7fYcW5UFgbJyHeC/view?usp=sharing'} target={'_blank'}>Скачать спецификацию</a>
                             <div className="download-icon">
                                 <svg width="28px" height="28px" viewBox="0 0 28 28" version="1.1" xmlns="http://www.w3.org/2000/svg">
                                     <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
@@ -100,24 +97,6 @@ class Pack extends Component {
                             <div className="demo-ipad">
                                 <div className="screen">
                                     <Gif gifUrl={opackVideo} isPlaying={this.state.playingAr} previewUrl={opackVideoPreview} />
-                                    {/*{ this.state.packPlaying || device.desktop() ?*/}
-                                    {/*    <img src={opackVideo} width={'100%'} alt={'oPack demonstration video.'}/>*/}
-                                    {/*    :*/}
-                                    {/*    <div className={'play-control'} onClick={this.playPack}>*/}
-                                    {/*        <div className="play-icon">*/}
-                                    {/*            <svg width="17px" height="21px" viewBox="0 0 17 21" version="1.1" xmlns="http://www.w3.org/2000/svg">*/}
-                                    {/*                <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">*/}
-                                    {/*                    <g id="Native-Robotics" transform="translate(-706.000000, -2104.000000)" fill="#FFFFFF" fillRule="nonzero">*/}
-                                    {/*                        <g id="oPack" transform="translate(-8.000000, 1609.000000)">*/}
-                                    {/*                            <path d="M724.06204,498.186584 L731.824252,510.70628 C732.406293,511.645057 732.117102,512.877923 731.178326,513.459965 C730.861831,513.656192 730.496836,513.760159 730.124446,513.760159 L714.600023,513.760159 C713.495454,513.760159 712.600023,512.864729 712.600023,511.760159 C712.600023,511.387769 712.703991,511.022775 712.900218,510.70628 L720.662429,498.186584 C721.244471,497.247807 722.477338,496.958616 723.416114,497.540658 C723.678368,497.703255 723.899443,497.92433 724.06204,498.186584 Z" id="Triangle" transform="translate(722.362372, 505.500174) rotate(90.000000) translate(-722.362372, -505.500174) "></path>*/}
-                                    {/*                        </g>*/}
-                                    {/*                    </g>*/}
-                                    {/*                </g>*/}
-                                    {/*            </svg>*/}
-                                    {/*        </div>*/}
-                                    {/*        <p>Watch</p>*/}
-                                    {/*    </div>*/}
-                                    {/*}*/}
                                 </div>
                             </div>
                         </div>
@@ -125,39 +104,39 @@ class Pack extends Component {
                     <Section id={'codegain'} background={"GreyWhite"}>
                         <SectionHeader>CodeGain</SectionHeader>
                         <SectionDescription>
-                            Solution for automatic generation of the program for robots in common applications. CodeGain is a base part of all OmniKit solutions.
+                            Технология для автоматической генерации исполняемой программы промышленных роботов. CodeGain лежит в основе платформы OmniKit и позволяет превратить компьютерную симуляцию в реальную программу для Вашего конкретного робота.
                         </SectionDescription>
-                        <SectionCaption>How does it work?</SectionCaption>
+                        <SectionCaption>Как это работает?</SectionCaption>
                         <div className="codegain-steps">
                             <div className="step">
                                 <div className="icon-terminal"></div>
-                                <p>Input all initial data related to the task.</p>
+                                <p>Введите информацию о задании.</p>
                             </div>
                             <div className="step">
                                 <div className="icon-pad"></div>
-                                <p>Preview generated program in simulation.</p>
+                                <p>Проверьте сгенерированную программу в симуляторе.</p>
                             </div>
                             <div className="step">
                                 <div className="icon-robot"></div>
-                                <p>Upload the program to robot.</p>
+                                <p>Отправьте программу в робота для исполнения.</p>
                             </div>
                         </div>
                     </Section>
                     <Section id={'coming-soon'} background={"Black"} dividerColor={'#000000'}>
                         <SectionHeader icon={oDev}/>
-                        <SectionHeader white={true} className={"smaller"} fontSize={32}>Coming soon with CodeGain</SectionHeader>
+                        <SectionHeader white={true} className={"smaller"} fontSize={32}>Скоро в CodeGain</SectionHeader>
                         <div className="soon-tabs">
                             <div className="tab">
                                 <h3>oBend</h3>
-                                <p>Solution for bending machines.</p>
+                                <p>Решение для гибочных машин.</p>
                             </div>
                             <div className="tab">
                                 <h3>oTend</h3>
-                                <p>Code generation for machine tending.</p>
+                                <p>Генерация кода для машинного обслуживания.</p>
                             </div>
                             <div className="tab">
                                 <h3>oWeld</h3>
-                                <p>Support of the welding applications.</p>
+                                <p>Поддержка сварочных приложений.</p>
                             </div>
                         </div>
                     </Section>
