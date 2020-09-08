1903a19b02fec6642c5a253417d0a9a7a6d453ee
allenWTF <niowen@mail.ru>
2020-09-08 15:24:50 +0300

    Updated GitHub Pages version to 0.1

--- responsiveMinimalTemplateLoader.js
+++ responsiveMinimalTemplateLoader.js
@@ -1,12 +1,6 @@
-/**
- * Created by miguelferreira on 02/09/2017.
- */
-
 var gameInstance = null;
 var gameContainer = null;
 var gameCanvas = null;
-var loadingContainer = null;
-var loadingText = null;
 var runtimeInitialized = false;
 var canvasAspectRatio = false;
 
@@ -39,37 +33,15 @@ function OnRuntimeIntialized() {
     handleResize();
 }
 
-function dummyProgressFunction() {
-}
 
 function getCanvasSize() {
     var windowWidth = window.innerWidth;
     var windowHeight = window.innerHeight;
 
-    if(canvasAspectRatio) {
-        var aspectWindowHeight = windowWidth / canvasAspectRatio;
-        if(aspectWindowHeight > windowHeight) {
-            windowWidth = windowHeight * canvasAspectRatio;
-        }
-        else {
-            windowHeight = aspectWindowHeight;
-        }
-    }
-
     return {width:windowWidth,height:windowHeight};
 }
 
-function instantiateUnity(url,aspectRatio) {
-
-    if(aspectRatio) {
-        var aspectRatioComponents = aspectRatio.split(":");
-        if(aspectRatioComponents.length != 2) {
-            console.exception("Unity: Aspect Ratio tag doesn't follow the expect aspect ratio format A:B e.g. 16:9")
-            return;
-        }
-
-        canvasAspectRatio = aspectRatioComponents[0]/aspectRatioComponents[1];
-    }
+function instantiateUnity(url) {
 
     var canvasSize = getCanvasSize();
 
